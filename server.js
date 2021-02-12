7eac5beab6897c411ddacf31e0d5d7b5f7a995c2
Manuel Leung Chen <manuel.leungchen@gmail.com>
2021-02-11 22:56:23 -0500

    Heroku and MongoDB Atlas Setup

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ app.use(express.static("public"));
 app.use(compression());
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 // routes
